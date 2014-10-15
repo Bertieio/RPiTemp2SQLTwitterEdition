6ade44089d6aefb2dfe4d2a99c74ba690998a9b2
Bertieio <bertie@bertie.io>
2014-10-15 20:56:13 +0100

    Same as RPiTemp2SQL but with twitter bot!

---
+++ README.md
@@ -0,0 +1,9 @@
+RPiTemp2SQL
+===========
+
+Requires: python-MySQLdb, Twython
+Basic program that gets the temp from a DS18B20, the Pi's CPU and the time then uploads it to a database
+
+Here is the webpage I use to show it: http://bertie.io/TempPrintR.php
+More info and set up can be found here: https://www.cl.cam.ac.uk/projects/raspberrypi/tutorials/temperature/
+added twitter bot see mine at @Temp_RPi
---
+++ TempSensor.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2012 Matthew Kirk
+# Licensed under MIT License, see
+# http://www.cl.cam.ac.uk/freshers/raspberrypi/tutorials/temperature/LICENSE
+# Edited to add database support 2014 Bertie Scott to be used under the same licence as above
+# https://www.cl.cam.ac.uk/projects/raspberrypi/tutorials/temperature/
+
+import MySQLdb
+import time
+import os, glob
+from twython import Twython, TwythonError
+
+APP_KEY = ''
+APP_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+# Login information contained below should be kept secure
+os.system("sudo modprobe w1-therm")
+os.system("sudo modprobe w1-gpio")
+while True:
+	Start = time.time()
+	db = MySQLdb.connect(host="",	# Your host, usually localhost
+		user="",		# Your username
+		passwd="",		# Your password
+		db="")			# Name of the data base
+
+	cur = db.cursor() 
+	SQLQ = "INSERT INTO TBL_Temp(Time, Temp, CPUTemp) VALUES (%s, %s, %s)"
+	tfile = open(glob.glob("/sys/bus/w1/devices/w1_bus_master1/"+"28*")[0]+"/w1_slave")	# Auto-finds your device
+	text = tfile.read()
+	tfile.close()
+	temperature_data = text.split()[-1]
+	temperature = float(temperature_data[2:])/1000.0
+	UTime =  int(time.time())
+	CPUTempC = "/opt/vc/bin/vcgencmd measure_temp > CPUTEMP"
+	os.system(CPUTempC)
+	CPUFile = open("CPUTEMP")
+	CPUText = CPUFile.read()
+	CPUFile.close()
+	TempCPU = CPUText[5:9]
+	TempCPU = eval(TempCPU)
+	tweet=text = 'Temp : {} C CPUTemp: {} C'.format(temperature,TempCPU)
+	twitter.update_status(status=tweet)
+	print tweet
+	print "TIME: " ,UTime
+	print "TEMP: " ,temperature
+	print "CPU TEMP: " ,TempCPU
+	cur.execute(SQLQ, (UTime, temperature, TempCPU))
+	db.commit()
+	End = time.time()
+	time.sleep(600-(End-Start))	# Loops every 600 seconds, depending on how long it took to execute
